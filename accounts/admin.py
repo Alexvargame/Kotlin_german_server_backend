1c342f5c2b5cc614e3646394e8fad87f65d93d01
Alexvargame <direct.fito@gmail.com>
2026-02-24 21:15:03 +0300

    Avatar_2_2_3

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from .models import User, EmailVerification, PhoneVerification, UserGalleryAvata
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'uid', 'username', 'score', 'streak_days', 'avatar_name',
+                    'avatar_path',
                     'last_login_date', 'is_verified', 'is_active', 'created_at')
     search_fields = ('email', 'uid')
     list_filter = ('is_verified', 'is_active')
