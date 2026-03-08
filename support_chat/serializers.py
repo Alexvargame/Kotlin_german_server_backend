288a027b099bf51bad5021971c7d68ab63d58ada
Alexvargame <direct.fito@gmail.com>
2026-03-08 19:55:16 +0300

    support_chat_12

--- support_chat/serializers.py
+++ support_chat/serializers.py
@@ -6,12 +6,16 @@ from django.utils import timezone
 class SupportMessageSerializer(serializers.ModelSerializer):
 
     reply_to_text = serializers.SerializerMethodField()
+    sender_UID = serializers.SerializerMethodField()
+    receiver_UID = serializers.SerializerMethodField()
 
     class Meta:
         model = Support_message
         fields = (
             'sender',
+            'sender_UID',
             'receiver',
+            'receiver_UID',
             'text',
             'reply_to_text',
             'created_at',
@@ -21,4 +25,14 @@ class SupportMessageSerializer(serializers.ModelSerializer):
     def get_reply_to_text(self, obj):
         if obj.reply_to:
             return obj.reply_to.text
+        return None
+
+    def get_receiver_UID(self, obj):
+        if obj.receiver:
+            return obj.receiver.uid
+        return None
+
+    def get_sender_UID(self, obj):
+        if obj.sender:
+            return obj.sender.uid
         return None
